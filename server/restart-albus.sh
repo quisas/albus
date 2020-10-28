c94fa0414e03a4d8cc70f3d6d078fa3462cc292d
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2020-10-28 11:23:27 +0100

    style. restart script.

--- server/restart-albus.sh
+++ server/restart-albus.sh
@@ -4,7 +4,9 @@ MYDIR=$(dirname "$0")
 
 $MYDIR/../cli_tools/runTask.sh restartPharoUsingMonit
 
-# Warten bis dieses pharo gestoppt ist. OPTIMIZE
-while pgrep -af "pharo.*albus_proplan.image" > /dev/null; do sleep 1; done
+# Wait until end of pharo's parent-process (pharo-ui script)
+while pgrep -f "pharo-ui.*albus_proplan.image" > /dev/null; do sleep 1; done
 
+# Revalidate monit after 5 seconds, this will restart pharo
+sleep 5
 sudo monit validate

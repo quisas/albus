a402c7475ccbf0a59f58c57170ef39010ac77212
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2021-04-27 15:38:09 +0200

    scripts

--- hashicorp_vault/start.sh
+++ hashicorp_vault/start.sh
@@ -5,7 +5,7 @@ DIR="/opt/albus/main/hashicorp_vault"
 PID_FILE="$DIR/vault.pid"
 
 # ./pharo-ui --encoding latin1 -vm-display-X11 albus_proplan.image startup.st > log/pharo_$NOW.log 2>&1  &
-vault server --config=config.hcl > vault.log 2>&1  &
+/usr/bin/vault server --config=config.hcl > vault.log 2>&1  &
 
 sleep 2
 

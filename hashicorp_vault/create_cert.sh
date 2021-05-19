3fdc853703c674e6e7ecc5f2f85a3a1d089e061a
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2021-05-19 15:37:10 +0200

    progress

--- hashicorp_vault/create_cert.sh
+++ hashicorp_vault/create_cert.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openssl req -x509 -newkey rsa:4096 -keyout albus_vault.key -out albus_vault.crt -days 365 -nodes -subj '/CN=localhost'
+openssl req -x509 -newkey rsa:4096 -keyout albus_vault.key -out albus_vault.crt -days 365 -nodes -subj '/CN=localhost' -config certconfig.conf
 
 sudo mkdir /usr/share/ca-certificates/albus_vault
 sudo cp albus_vault.crt /usr/share/ca-certificates/albus_vault/albus_vault-ca.crt

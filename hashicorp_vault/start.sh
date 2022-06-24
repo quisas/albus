8982be73bb81ce9843ba1090f48d48168d2fab98
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2022-06-24 14:12:25 +0200

    progress

--- hashicorp_vault/start.sh
+++ hashicorp_vault/start.sh
@@ -4,7 +4,6 @@
 DIR="/opt/albus/main/hashicorp_vault"
 PID_FILE="$DIR/vault.pid"
 
-# ./pharo-ui --encoding latin1 -vm-display-X11 albus_proplan.image startup.st > log/pharo_$NOW.log 2>&1  &
 /usr/bin/vault server --config=config.hcl > vault.log 2>&1  &
 
 sleep 2

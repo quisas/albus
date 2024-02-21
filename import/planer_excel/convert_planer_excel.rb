fdfdf43f52dd1d813d1d201de2639aac896f36d8
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2024-02-21 16:15:16 +0100

    progress

--- import/planer_excel/convert_planer_excel.rb
+++ import/planer_excel/convert_planer_excel.rb
@@ -1,11 +1,49 @@
 # coding: utf-8
-require 'rubygems'
 
+###############################################################################################
+# Planer-Excel Import
+#
+
+# HINWEIS! Es braucht eine modifizierte Version von spreadsheet (in vendor/modified_gems/...), welche schlicht den Zahlenwert für pattern_bg_color gibt
+# und dies nicht in benannte Farbnamen umwandeln will (und nicht kann, wenn unbekannte Farben gewählt wurden)
+
+# Vorbereitung:
+# - Excel-Farbtabelle muss angepasst werden mit den korrekten Fachkürzeln!
+# - Excel-File muss im Format Excel 97/2000 gespeichert werden, sonst werden die Farben nicht gelesen.
+
+# 2023 Wir lesen optional keine Farben mehr
+
+# Aufbau im Excel:
+# - Jede Zelle enthält 1 oder mehrere Angaben im Format LP[,LP][/Fach[-HK][/Fachgruppe]]
+# - HK = Halbklasse
+# - Farbe wird optional eingelesen als Fach, falls es nicht schon genannt wurde
+
+
+# TODO: Teils veraltet
+# Regeln des Imports:
+# - Fachlegende einlesen und loggen. Nur Fächer matchen, welche ein korrektes Kürzel haben. Andere ignorieren und loggen.
+# - Wenn Fach per Farbe nicht erkannt wird, oder wenn mehrere Lehrer in einer Zelle drin stehen, dann wird das Fach aufgrund der bestehenden
+#   Skelettlektionen indirekt herausgefunden und geloggt.
+# - Leere Zellen komplett ignorieren
+# - Graue Zellen werden ignoriert (PK, FF) und müssen manuell in ProPlan gesetzt werden (wir können das nicht importieren, zu wenig Informationen)
+
+require 'rubygems'
 $LOAD_PATH.push('./modified_gems/spreadsheet-1.1.2/lib')
 require 'spreadsheet'
 
 
-FileUtils.rm_f('convert.log')
+###############################################################################################
+# Config
+#
+
+# Farbangaben in Excel soll als Fach eingelesen werden? (Default: false, nur Charlotte verwendet das evt.)
+$use_colors = true
+
+
+
+###############################################################################################
+# Lib
+#
 
 def import_logger
   $Import_logger ||= Logger.new("convert.log")
@@ -68,30 +106,13 @@ def add_csv(klasse, subject, halbklasse, subjects_group, teacher, day, lesson)
   # puts(row)
 end
 
-###############################################################################################
-# Planer-Excel Import
-#
-
-# HINWEIS! Es braucht eine modifizierte Version von spreadsheet (in vendor/modified_gems/...), welche schlicht den Zahlenwert für pattern_bg_color gibt
-# und dies nicht in benannte Farbnamen umwandeln will (und nicht kann, wenn unbekannte Farben gewählt wurden)
-
-# Vorbereitung:
-# - Excel-Farbtabelle muss angepasst werden mit den korrekten Fachkürzeln!
-# - Excel-File muss im Format Excel 97/2000 gespeichert werden, sonst werden die Farben nicht gelesen.
-
-# 2023 TODO: Wir lesen keine Farben mehr, deshalb kann evt. das oben beschriebene vereinfacht werden?!?
-
-
-# Regeln des Imports:
-# - Fachlegende einlesen und loggen. Nur Fächer matchen, welche ein korrektes Kürzel haben. Andere ignorieren und loggen.
-# - Wenn Fach per Farbe nicht erkannt wird, oder wenn mehrere Lehrer in einer Zelle drin stehen, dann wird das Fach aufgrund der bestehenden
-#   Skelettlektionen indirekt herausgefunden und geloggt.
-# - Leere Zellen komplett ignorieren
-# - Graue Zellen werden ignoriert (PK, FF) und müssen manuell in ProPlan gesetzt werden (wir können das nicht importieren, zu wenig Informationen)
 
 ###################################################################################################
 # Init
 #
+
+# Altes Logfile löschen
+FileUtils.rm_f('convert.log')
 
 log('Import aus Stundenplaner-Excel gestartet')
 
@@ -104,50 +125,51 @@ sheet = book.worksheet(0)
 #ignored_colors = ['2860', '2871', '2998', '5654', '3359', '3990', '5407'] # Grautöne
 
 
-# Ab 2023 FS verwenden wir die Farbe nicht mehr. Jacqueline schreibt im Text das Fach immer explizit
-# ###############################
-# # Farbtabelle einlesen
-# #
+# Ab 2023 FS verwenden wir die Farbe nicht mehr. Jacqueline schreibt im Text das Fach immer explizit. Charlotte verwendet es aber evt. trotzdem noch.
+###############################
+# Farbtabelle einlesen
+#
 
-# # Farbtabelle muss ab Zeile 2 beginnen und in Spalte BF/BG sein
+if $use_colors then
 
-# # Struktur: {Farbwert => Fach}
-# color_subject_map = {}
+  # Farbtabelle muss ab Zeile 2 beginnen und in Spalte BF/BG sein
 
-# COLOR_COLUMN_INDEX = 57 # Spalte BF
-# SUBJECT_COLUMN_INDEX = 58 # Spalte BG
-# row_index = 1
+  # Struktur: {Farbwert => Fach}
+  color_subject_map = {}
 
-# log("Farbzuweisungs-Tabelle:")
+  COLOR_COLUMN_INDEX = 57 # Spalte BF
+  SUBJECT_COLUMN_INDEX = 58 # Spalte BG
+  row_index = 1
 
-# # Wir suchen den Anfang der Tabelle
-# while true do
-#   subject_code = sheet.row(row_index)[SUBJECT_COLUMN_INDEX].to_s.gsub("\n", '')
-#   break if not subject_code.empty?
-#   row_index += 1
-#   if row_index > 100
-#     log('Keine Farbtabelle gefunden. Muss in Spalte BF/BG seind')
-#     exit
-#   end
-# end
+  log("Farbzuweisungs-Tabelle:")
 
-# while true do
-#   color = sheet.row(row_index).format(COLOR_COLUMN_INDEX).pattern_bg_color.to_s
-#   subject_code = sheet.row(row_index)[SUBJECT_COLUMN_INDEX].to_s.gsub("\n", '')
-#   subject_code.gsub!('(f)', '')
+  # Wir suchen den Anfang der Tabelle
+  while true do
+    subject_code = sheet.row(row_index)[SUBJECT_COLUMN_INDEX].to_s.gsub("\n", '')
+    break if not subject_code.empty?
+    row_index += 1
+    if row_index > 100
+      log('Keine Farbtabelle gefunden. Muss in Spalte BF/BG seind')
+      exit
+    end
+  end
 
-#   # Wir gehen durch die Tabelle, bis zum ersten leeren Feld
-#   break if subject_code.empty?
-  
-#   raise "Doppelte Farbe #{color}!" if color_subject_map.has_key?(color)
-#   color_subject_map[color] = subject_code
-#   log("#{color} zu #{subject_code}")
-  
-#   row_index += 1
+  while true do
+    color = sheet.row(row_index).format(COLOR_COLUMN_INDEX).pattern_bg_color.to_s
+    subject_code = sheet.row(row_index)[SUBJECT_COLUMN_INDEX].to_s.gsub("\n", '')
+    subject_code.gsub!('(f)', '')
 
-# end
+    # Wir gehen durch die Tabelle, bis zum ersten leeren Feld
+    break if subject_code.empty?
+    
+    raise "Doppelte Farbe #{color}!" if color_subject_map.has_key?(color)
+    color_subject_map[color] = subject_code
+    log("#{color} zu #{subject_code}")
+    
+    row_index += 1
 
-
+  end
+end
 
 ###################################################################################################
 # Zellen einlesen
@@ -174,37 +196,38 @@ while go_ahead
 
         index = ((day_index - 1) * 11) + lesson_index
         column_index = index + lesson_start_offset
-        text = row[column_index]
+        text = row[column_index].to_s.strip
 
         # tick("Verarbeite Zelle #{cell_name(column_index, row_index)} mit Inhalt '#{text}'", row_index, column_index)
         tick("Verarbeite Zelle #{cell_name(column_index, row_index)}", row_index, column_index)
       
         # Wenn Zelle leer, dann weiter
-        next if text.to_s.strip.empty?
+        next if text.empty?
 
         # Explizit unbekannte Details. Ignorieren.
-        next if text.to_s.strip == '???'
+        next if text == '???'
         
-        # # Fach rausfinden anhand der Farbe
-        # color = row.format(column_index).pattern_bg_color.to_s
-      
-        # # Wenn wir das Fach nicht erkennen (bei einem nicht-leeren Feld, an dieser Stelle)
-        # if color_subject_map.has_key?(color)
-        #   subject = color_subject_map[color]
-        #   invalid_color = false
-        # else
-        #   subject = ''
-        #   unless ignored_colors.include?(color)
-        #     invalid_color = true
-        #   end
-        # end
-
+        # Optional: Fach rausfinden anhand der Farbe
+        if $use_colors then
+          color = row.format(column_index).pattern_bg_color.to_s
+          
+          # Wenn wir das Fach nicht erkennen (bei einem nicht-leeren Feld, an dieser Stelle)
+          if color_subject_map.has_key?(color)
+            subject_from_color = color_subject_map[color]
+            invalid_color = false
+          else
+            #unless ignored_colors.include?(color)
+            invalid_color = true
+            #end
+          end
+        end
 
         # Vorberechnung aller Lehrer. Nicht ganz optimiert und redundant weil unten nochmals, aber pragmatisch.
         # Lehrer rausfinden, anhand der Kürzel
-        # Split nur bei Zeilenumbruch, so dass Leerzeichen in Fachnamen möglich sind
+
+        # Variante: Split nur bei Zeilenumbruch, so dass Leerzeichen in Fachnamen möglich sind:
 #        tokens = text.split(/\r?\n/) #Falls es Fachnamen mit Leerzeichen hat, dann müssen wir strikt Zeilen trennen. Siehe "Conv. francaise" etc.
-        tokens = text.split(/[\r|\n|\s]/)
+        tokens = text.split(/[\r|\n|\s]/) # Split bei Zeilenumbruch ODER Leerschlag
         
         teachers = []
         for token in tokens
@@ -252,15 +275,17 @@ while go_ahead
           # end
 
 
-          # # Fach von Farbe nehmen, falls nichts angegeben im Token
-          # if token_subject.empty?
-          #   token_subject = subject
+          # Optional: Fach von Farbe nehmen, falls nichts angegeben im Token
+          if $use_colors then
+            if token_subject.empty?
+              subject = subject_from_color
 
-          #   if invalid_color
-          #     log_cell("Fach nicht aus Farbe erkannt und fehlende Fachangabe. Farbe #{color}, Zellinhalt: #{text}")
-          #   end
-            
-          # end
+              if invalid_color
+                log_cell("Fach nicht aus Farbe erkannt und fehlende Fachangabe. Farbe #{color}, Zellinhalt: #{text}")
+              end
+              
+            end
+          end
 
         
           # In CSV schreiben

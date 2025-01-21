a653f2c94d558f85328ee1be19b42f41d47f7971
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2025-01-21 10:43:08 +0100

    script

---
+++ cli_tools/microsoft-login.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+JSON=$(curl -s --max-time 5 -X GET -H "Authorization: Bearer $1" -H "Content-Type: application/json; charset=utf-8" https://graph.microsoft.com/v1.0/me)
+
+# echo $JSON
+
+# echo $JSON | jq -e -r ".mail,.givenName,.surname"
+echo $JSON | jq -e -r ".mail"
